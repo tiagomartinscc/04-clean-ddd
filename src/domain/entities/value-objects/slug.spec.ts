780c739de73ec1fb272ae5e92effa0b207255f35
Tiago Martins <tiagomartinscc@hotmail.com>
2024-12-14 22:15:30 -0300

    create a slug value object

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from "vitest"
+import { Slug } from "./slug"
+
+test('it should be able to create a new slug from text', () => {
+  const slug = Slug.createFromText('Example text title')
+  expect(slug.value).toEqual('example-text-title')
+})
