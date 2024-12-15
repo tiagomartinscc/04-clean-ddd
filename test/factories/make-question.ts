24e0b781d7d777568748a156ebe62e71e8587067
Tiago Martins <tiagomartinscc@hotmail.com>
2024-12-15 18:42:55 -0300

    create a factorie pattern to make a question using faker

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,15 +1,19 @@
+import { faker } from '@faker-js/faker'
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
 
-export function makeQuestion(override: Partial<QuestionProps> = {})
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID
+)
 {
   const question = Question.create({
     authorId: new UniqueEntityID(),
-    title: 'Example Question',
+    title: faker.lorem.sentence(),
     slug: Slug.create('example-question'),
-    content: 'Example content',
+    content: faker.lorem.text(),
     ...override
-  })
+  }, id)
   return question
 }
