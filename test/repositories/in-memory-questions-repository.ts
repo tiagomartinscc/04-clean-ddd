e7fb134335dae017c507635973bf68edb24dd618
Tiago Martins <tiagomartinscc@hotmail.com>
2024-12-15 19:10:48 -0300

    delete question use case

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,21 +1,36 @@
-import { QuestionsRepository } 
-  from "@/domain/forum/application/repositories/questions-repository";
-import { Question } from "@/domain/forum/enterprise/entities/question";
+import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
 
-export class InMemoryQuestionsRepository implements QuestionsRepository 
-{
+export class InMemoryQuestionsRepository implements QuestionsRepository {
   public items: Question[] = []
 
-  async findBySlug(slug: string) {
-    const question = this.items.find(item => item.slug.value === slug)
+  async findById(id: string) {
+    const question = this.items.find((item) => item.id.toString() === id)
 
     if (!question) {
       return null
     }
+
     return question
   }
 
-  async create(question: Question): Promise<void> {
+  async findBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
+  async create(question: Question) {
     this.items.push(question)
+  }
+
+  async delete(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+
+    this.items.splice(itemIndex, 1)
   }
 }
