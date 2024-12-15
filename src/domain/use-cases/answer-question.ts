a59c8dc96f4c32be98663ef671bd4b0e1f46471a
Tiago Martins <tiagomartinscc@hotmail.com>
2024-12-15 14:45:32 -0300

    mapping prop

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { Answer } from "../entities/answer"
 import { AnswerRepository } from "../repositories/answer-repository"
 interface AnswerQuestionUseCaseRequest {
@@ -12,12 +13,12 @@ export class AnswerQuestionUseCase {
   ) {}
 
   async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer({
+    const answer = Answer.create({
       content, 
-      authorId: instructorId, 
-      questionId})
+      authorId: new UniqueEntityID(instructorId), 
+      questionId: new UniqueEntityID(questionId)})
 
-      await this.answerRepository.create({ answer })
+      await this.answerRepository.create(answer)
     return answer
   }
 }
