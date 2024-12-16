73fcaf37de598be3a9b1d721e6662804f2b328ae
Tiago Martins <tiagomartinscc@hotmail.com>
2024-12-15 21:46:05 -0300

    delete answer

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -7,6 +7,22 @@ export class InMemoryAnswersRepository implements AnswersRepository
 {
   public items: Answer[] = []
 
+  async findById(id: string) {
+    const answer = this.items.find((item) => item.id.toString() === id)
+
+    if (!answer) {
+      return null
+    }
+
+    return answer
+  }
+
+  async delete(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items.splice(itemIndex, 1)
+  }
+
   async create(answer: Answer): Promise<void> {
     this.items.push(answer)
   }
